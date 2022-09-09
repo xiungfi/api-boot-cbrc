377d0c29cf5d156a7e02705aef567267da7af110
randeexiong <randeexiong@163.com>
2022-09-09 13:04:13 +0800

    去除所有无关

--- cbrc-statement-verify-services/src/main/java/org/minbox/framework/api/boot/cbrc/stateverify/CbrcStateVerifyApplication.java
+++ cbrc-statement-verify-services/src/main/java/org/minbox/framework/api/boot/cbrc/stateverify/CbrcStateVerifyApplication.java
@@ -1,14 +1,10 @@
 package org.minbox.framework.api.boot.cbrc.stateverify;
 
 import org.minbox.framework.api.boot.autoconfigure.swagger.annotation.EnableApiBootSwagger;
-import org.minbox.framework.logging.spring.context.annotation.client.EnableLoggingClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-
-import com.cxytiandi.encrypt.springboot.annotation.EnableEncrypt;
 
 /**
  *
@@ -18,9 +14,6 @@ import com.cxytiandi.encrypt.springboot.annotation.EnableEncrypt;
  */
 @SpringBootApplication
 @EnableApiBootSwagger
-@EnableLoggingClient
-@EnableEncrypt
-@EnableAsync
 public class CbrcStateVerifyApplication {
     /**
      * logger instance
